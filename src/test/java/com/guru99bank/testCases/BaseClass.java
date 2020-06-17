4d17402842c28658fd332d997d7b95c8dfb2e5e7
Mahesh <solkanmahesh@gmail.com>
2020-06-17 16:17:42 +0530

    Implemented Log4j for the Project

--- src/test/java/com/guru99bank/testCases/BaseClass.java
+++ src/test/java/com/guru99bank/testCases/BaseClass.java
@@ -1,5 +1,7 @@
 package com.guru99bank.testCases;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -11,14 +13,18 @@ public class BaseClass
 	public String username="mngr265426";
 	public String password="AzUdUde";
 	public static WebDriver driver;
-	
+	public static Logger log;
+
 	@BeforeClass
 	public void setUp()
 	{
 		String path=System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", path+"//Drivers//chromedriver.exe");
 		driver=new ChromeDriver();
+		log=Logger.getLogger("Guru99BankDemo_V1");
+		PropertyConfigurator.configure("Log4j.properties");
 	}
+	
 	@AfterClass
 	public void tearDown()
 	{
--- src/test/java/com/guru99bank/testCases/TC_LoginTest_001.java
+++ src/test/java/com/guru99bank/testCases/TC_LoginTest_001.java
@@ -12,17 +12,23 @@ public class TC_LoginTest_001 extends BaseClass
 	public void loginTest() throws InterruptedException
 	{
 		driver.get(baseURL);
+		log.info("URL is Opened");
 		LoginPageObjs lpo=new LoginPageObjs(driver);
 		lpo.setUserName(username);
+		log.info("Entered Username");
 		lpo.setPassword(password);
+		log.info("Entered Password");
 		lpo.clickSubmit();
+		log.info("Clicked on Submit in Login Page");
 		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
 		{
 			Assert.assertTrue(true);
+			log.info("Login Test is Passed");
 		}
 		else
 		{
 			Assert.assertTrue(false);
+			log.info("Login Test is Failed");
 		}
 	}
 }
